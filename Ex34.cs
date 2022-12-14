8cd4225bffcd7c84f2ef1497d6208f64eb423319
Katrine-GitH <119107912+Katrine-GitH@users.noreply.github.com>
2022-12-14 23:45:29 +0300

    Add files via upload

---
+++ Ex34.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+//Создать массив наполненный положительными 3х значными цифарми
+int[] Three_digit_array = new int[4];
+int Even = 0;
+for (int i = 0; i < 4; i++)
+{
+    {
+        Three_digit_array[i] = new Random().Next(100, 999);
+        // Console.WriteLine($"{Three_digit_array[i]}");
+    }
+    Console.WriteLine($"Three_digit_array {i}:{Three_digit_array[i]}");
+}
+// //показать массив
+for (int i = 0; i < 4; i++)
+{
+    if (Three_digit_array[i] % 2 == 0)
+    {
+        Even += 1;
+    }
+    //Console.WriteLine($"i:{i}");
+}
+Console.WriteLine($"Even:{Even}");
+//ввод переменной хранящей количество четных чисел
+//показать эту переменную
---
+++ Ex39.cs
@@ -0,0 +1,48 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+double[] Array = new double[5];
+//{3,7,22,2,78};
+Random r = new Random();
+int i = 0;
+double difference = 0;
+while (i < 5)
+
+{
+    Array[i] = r.NextDouble() * 100;
+    Console.WriteLine($"Array {i} = {Array[i]}");
+    i++;
+}
+i = 0;
+double min = Array[0];
+double max = Array[0];
+while (i < 4)
+{
+    if (Array[i] > max)
+    {
+        max = Array[i];
+    }
+    else
+    if (Array[i] < min)
+    {
+        min = Array[i];
+        //Console.WriteLine($"difference= {difference}");
+    }
+    i++;
+    difference = max - min;
+}
+Console.WriteLine($"difference= {difference}");
+
+//Difference = max - min;
+
+// double[,] a = new double[5, 10];
+
+// Random random = new Random();
+// for (int i = 0; i < 5; i++)
+// {
+//     for (int j = 0; j < 10; j++)
+//     {
+//         a[i, j] = random.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+//         Console.Write("{0,6:F2}", a[i, j]);
+//     }
+//     Console.WriteLine();
+// }
