c73886621cffae0c927c2f5aa2c0f06c3a15eaff
Katrine-GitH <119107912+Katrine-GitH@users.noreply.github.com>
2022-12-14 23:43:07 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
-// которая покажет количество чётных чисел в массиве.
-//[345, 897, 568, 234] -> 2
-//Создать массив наполненный положительными 3х значными цифарми
-int[] Three_digit_array = new int[4];
-int Even = 0;
-for (int i = 0; i < 4; i++)
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+//int[] Three_digit_array = new int[4];
+int sum = 0;
+int[] Array = new int[7];
+Random r = new Random();
+for (int i = 0; i < 7; i++)
 {
+    Array[i] = r.Next(1, 10);
+    Console.WriteLine($"Array[{i}]:{Array[i]}");
+    if (i % 2 != 0)
     {
-        Three_digit_array[i] = new Random().Next(100, 999);
-        // Console.WriteLine($"{Three_digit_array[i]}");
+        sum += Array[i];
     }
-    Console.WriteLine($"Three_digit_array {i}:{Three_digit_array[i]}");
 }
-// //показать массив
-for (int i = 0; i < 4; i++)
-{
-    if (Three_digit_array[i] % 2 == 0)
-    {
-        Even += 1;
-    }
-    //Console.WriteLine($"i:{i}");
-}
-Console.WriteLine($"Even:{Even}");
-//ввод переменной хранящей количество четных чисел
-//показать эту переменную
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях :{sum}");
+
